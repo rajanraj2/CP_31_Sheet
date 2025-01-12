6ddb8ebf2873996b8a40311a1d790aa2b57d08ca
zorax <rajanraj253545@gmail.com>
2025-01-12 13:10:33 +0530

    Add few basic templates

---
+++ Templates/03_vector_n.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, x;
+        cin >> n >> x;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        
+    }    
+    return 0;
+}
