a7498ccade9e504455b468880e739fe4f4ebca7c
zorax <rajanraj253545@gmail.com>
2025-02-04 13:22:18 +0530

    Add rating 800 problem 22

--- 800/22_Coins.cpp
+++ 800/22_Coins.cpp
@@ -5,13 +5,12 @@ int main() {
     int t;
     cin >> t;
     while (t--) {
-        int n, k;
+        long long n, k;
         cin >> n >> k;
-        
+
         string result = "NO";
-        int diff = n - k;
-        if (n % 2 == 0 || n % k == 0 || n % (k + 2) == 0) result = "YES";
-        else if (diff >= 0 && diff % 2 == 0) result = "YES";
+        if (n % 2 == 0) result = "YES";
+        else if (k % 2 == 1 && k <= n) result = "YES";
         cout << result << endl;
     }    
     return 0;
