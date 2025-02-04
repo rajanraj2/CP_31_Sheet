d75a5b1bfb6e5132f3a49f5927d10be4a4fa9659
zorax <rajanraj253545@gmail.com>
2025-02-04 11:58:16 +0530

    Add rating 800 problem 13

--- 800/13_United_We_Stand.cpp
+++ 800/13_United_We_Stand.cpp
@@ -17,20 +17,35 @@ int main() {
         int n;
         cin >> n;
         vector<int> arr(n);
-        for (int i = 0; i < n; i++) {
-            cin >> arr[i];
-        }
+        map<int, int> mpp;
         vector<int> a, b;
+        int mini = INT_MAX;
 
         for (int i = 0; i < n; i++) {
-            if (isDivisor(a, arr[i])) {
-                b.push_back(arr[i]);
-            } 
-            else {
-                a.push_back(arr[i]);
-            }
+            cin >> arr[i];
+            mpp[arr[i]]++;
+            mini = min(mini, arr[i]);
         }
         
+        if (mpp.size() <= 1) {
+            cout << -1 << endl;
+            continue;
+        }
+
+        for (auto it : mpp) {
+            if (it.first == mini) {
+                for (int i = 0; i < it.second; i++) a.push_back(it.first);
+            }
+            else {
+                for (int i = 0; i < it.second; i++) b.push_back(it.first);
+            }
+        }
+
+        cout << a.size() << " " << b.size() << endl;
+        for (auto it : a) cout << it << " ";
+        cout << endl;
+        for (auto it : b) cout << it << " ";
+        cout << endl;
 
     }    
     return 0;
